57eb37c363037e7f5a32ce74cc195eaf033a147a
alex1998dmit <alex1998dmit@gmail.com>
2019-05-16 13:50:07 +0300

    Refactore

--- app/public/scripts.js
+++ app/public/scripts.js
@@ -12,355 +12,304 @@ function generateRandomSeq() {
     let max = 65555;
     return  Math.round( min - 0.5 + Math.random() * (max - min + 1));
 }
-
 // -----------------------------
 // Start state 
+class Main {
 
-class StartState {
     constructor() {
-        this.signState = "Начальное состояние, подготовка к соединению, создание исходного порта для связи с известным удаленным портом";
+        this.state = new BeforeStart(this);
+        this.signState = "";
         this.ipClient = "172.19.4.82";
         this.portClient = "54822";
         this.ipServer = "195.19.137.68";
-        this.portServer = "110";        
-        clearArea();
-        this.init();        
+        this.portServer = "110";
+        this.seq = 0;
+        this.ack = '-';
+        this.side = "client";
+        this.arrow = "first";
+        this.cli = [];
+        this.srcPort = this.portClient;
+        this.dstPort = this.portServer;
+        this.clientStatus = "";
+        this.serverStatus = "";
     }
 
-    init() {
-        this.changeIP();
+    clickFirstButton() {
+        clearArea();
+        this.state.clickFirstButton();
+    }
+
+    clickSecondButton() {
+        clearArea();
+        this.state.clickSecondButton();
+    }
+
+    render(){
         this.changeStatusSign();
-        this.addButtons();
+        this.showIPs();
         this.showSide();
         this.showArrow();
+        this.changeTable();
+        this.changeClientStatus();
+        this.changeServerStatus();
     }
 
-    changeIP() {        
+    changeStatusSign() {
+        $("#status-signs").html(`<h5 class="state-sign">${this.signState}</h5>`);
+    }
+
+    showIPs() {        
         $("#clientId").text(`${this.ipClient}:${this.portClient}`);
         $("#serverId").text(`${this.ipServer}:${this.portServer}`);
     }
 
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-    addButtons() {
-        $("#start-state-button").append(`<button type="button" id="send-first-flag-button" class="btn btn-primary">Send SYN</button>`);
-    }
-
     showSide() {
-        $(".client-sign").css("border-bottom", "4px solid green");	
+        $(`.${this.side}-sign`).css("border-bottom", "4px solid green");	 
     }
 
     showArrow() {
-        $(".arrow-first").css("opacity", "1");
-    }
-}
-
-// ---------------------------------------------------------------------------------
-
-class SendFirstFlags {
-    constructor(seq, portClient, portServer) {
-        this.signState = "Клиент выполнил запрос на создание TCP-сессии. Отправлен SYN-запроc)";
-        this.seq = seq;
-        this.srcPort = portClient;
-        this.dstPort = portServer;
-        this.cli = ["SYN"];
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        this.changeStatusSign();
-        // this.addButtons();
-        this.showSide();
-        this.changeTable();
-        this.showAcceptButton();
-        this.showSide();
-        this.showRejectConnectionButton();
-        this.changeServerStatus();
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-    showArrow() {
-        $(".arrow-first").css("opacity", "1");
-    }
-
-    changeTable() {
-        $(".table-seq").text(`${this.seq}`);
-        $(".table-src-port").text(`${this.srcPort}`);
-        $(".table-dst-port").text(`${this.dstPort}`);
-        $(".table-cli").text(`${this.cli.join(', ')}`);
-    }
-
-    showRejectConnectionButton() {
-        $("#start-state-button").append(`<button type="button" id="reject-syn-server" class="btn btn-danger">Reject</button>`);
-    }
-
-    showAcceptButton() {
-        $("#start-state-button").append(`<button type="button" id="received-syn-server" class="btn btn-success">Accept</button>`);
-    }    
-
-    showSide() {
-        $(".server-sign").css("border-bottom", "4px solid green");	
-    }
-
-    changeServerStatus() {
-        $(".client-sign h6").text(`SEND-SYN`);
-    }
-}
-
-// ---------------------------------------------------------------------------------
-
-class ServerReceived {
-    constructor() {
-        this.signState = "Сервер получил сегмент, запомнил номер последовательности и создал сокет для обслуживания нового клиента.";
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        this.changeStatusSign();
-        this.showSendPocketsToClientWithAckAndSyn();
-        this.showSide();
-        this.changeServerStatus();
-        this.showArrow();
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-    showSendPocketsToClientWithAckAndSyn() {
-        $("#start-state-button").append(`<button type="button" id="send-syn-ack" class="btn btn-success">Send ACK SYN</button>`);
-    } 
-    
-    showSide() {
-        $(".server-sign").css("border-bottom", "4px solid green");	
-    }
-
-    changeServerStatus() {
-        $(".server-sign h6").text(`SYN-RECEIVED`);
-    }
-
-    showArrow() {
-        $(".arrow-second").css("opacity", "1");
-    }
-
-}
-
-// ---------------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------------
-class ServerRejectSynState {
-    constructor() {
-        this.signState = "Сервер не принял запрос";
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        // this.changeTable();
-        this.showButtonSendRst();
-        this.changeStatusSign();
-        this.showArrow();
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-    showButtonSendRst() {
-        $("#start-state-button").append(`<button type="button" id="send-rst" class="btn btn-success">Send RST</button>`);
-        $("#start-state-button").append(`<button type="button" id="wait-nothing" class="btn btn-info">Do nothing</button>`);
-    }
-
-    showArrow() {
-        $(".arrow-second").css("opacity", "1");
-    }
-
-}
-
-// ---------------------------------------------------------------------------------
-
-class ClientGetRstFlag {
-    constructor(seq, portClient, portServer) {
-        this.signState = "Клиент получил RST-флаг, соединение разорвано";
-        this.seq = seq;
-        this.srcPort = portClient;
-        this.dstPort = portServer;
-        this.cli = ["RST"];
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        this.changeTable();
-        this.changeStatusSign();
-    }
-
-    changeTable() {
-        $(".table-seq").text(`${this.seq}`);
-        $(".table-src-port").text(`${this.dstPort}`);
-        $(".table-dst-port").text(`${this.srcPort}`);
-        $(".table-cli").text(`${this.cli.join(', ')}`);
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-}
-
-// ---------------------------------------------------------------------------------
-
-class ClientGetAckAndSyn  {
-    constructor(seq, portClient, portServer, ack) {
-        this.signState = "Клиент получил сегмент с ACK и SYN";
-        this.seq = seq + 1;
-        this.ack = ack;
-        this.srcPort = portClient;
-        this.dstPort = portServer;
-        this.cli = ["ACK, SYN"];
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        this.changeTable();
-        this.changeStatusSign();
-        this.showArrow();  
-        this.showButtonSendToServer();    
-        this.changeServerStatus();
-        this.showSide();  
-        this.chageClientStatus();
-    }
-
-    changeTable() {
-        $(".table-seq").text(`${this.ack}`);
-        $(".table-ack").text(`${this.seq}`);
-        $(".table-src-port").text(`${this.dstPort}`);
-        $(".table-dst-port").text(`${this.srcPort}`);
-        $(".table-cli").text(`${this.cli.join(', ')}`);
-    }
-
-    showArrow() {
-        $(".arrow-third").css("opacity", "1");
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
-    }
-
-    showButtonSendToServer() {
-        $("#start-state-button").append(`<button type="button" id="final-state" class="btn btn-info">Send ACK</button>`);
-    }
-
-    showSide() {
-        $(".client-sign").css("border-bottom", "4px solid green");	
-    }
-
-    changeServerStatus() {
-        $(".server-sign h6").text(`SEND-SYN`);
-    }
-
-    chageClientStatus() {
-        $(".client-sign h6").text(`SYN-RECEIVED`);
-    }
-}
-
-// =-----------------------------------------------
-
-class EstablishedServer{
-    constructor(seq, portClient, portServer, ack) {
-        this.signState = "Сервер в состоянии SYN-RECEIVED получил сегмент с флагом ACK. Соединение установлено";
-        this.seq = seq + 1;
-        this.ack = ack + 1;
-        this.srcPort = portClient;
-        this.dstPort = portServer;
-        this.cli = ["ACK"];
-        clearArea();
-        this.init();
-    }
-
-    init() {
-        // this.changeTable();
-        this.changeStatusSign();
-        this.showArrow();  
-        this.changeTable();
-        this.changeServerStatus();
-        this.chageClientStatus();
+        $(`.arrow`).css("opacity", 0.1);
+        $(`.arrow-${this.arrow}`).css("opacity", "1");
     }
 
     changeTable() {
         $(".table-seq").text(`${this.seq}`);
         $(".table-ack").text(`${this.ack}`);
-        $(".table-src-port").text(`${this.dstPort}`);
-        $(".table-dst-port").text(`${this.srcPort}`);
+        $(".table-src-port").text(`${this.srcPort}`);
+        $(".table-dst-port").text(`${this.dstPort}`);
         $(".table-cli").text(`${this.cli.join(', ')}`);
     }
 
-    showArrow() {
-        $(".arrow-two-side").css("opacity", "1");
-    }
-
-    changeStatusSign() {
-        $("#status-signs").append(`<h5 class="state-sign">${this.signState}</h5>`);
+    changeClientStatus() {
+        $(".client-sign h6").text(this.clientStatus);
     }
 
     changeServerStatus() {
-        $(".server-sign h6").text(`Established`);
+        $(".server-sign h6").text(this.serverStatus);
     }
 
-    chageClientStatus() {
-        $(".client-sign h6").text(`Established`);
+    setParamsDefault() {
+        this.signState = "";
+        this.ipClient = "172.19.4.82";
+        this.portClient = "54822";
+        this.ipServer = "195.19.137.68";
+        this.portServer = "110";
+        this.seq = 0;
+        this.ack = 0;
+        this.side = "client";
+        this.arrow = "first";
+        this.cli = [];
+        this.srcPort = this.portClient;
+        this.dstPort = this.portServer;
+        this.clientStatus = "";
+        this.serverStatus = "";     
+    }
+}
+// -------------------------------------------------------------------
+class BeforeStart {
+    constructor(mainState) {
+        this.mainState = mainState;
+    }
+
+    clickFirstButton() {
+        this.mainState.state = new StartState(this.mainState);
+        this.mainState.render();      
+    }
+}
+// -------------------------------------------------------------------
+class StartState {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+        this.addButtonForSynRequest();
+    }
+
+    setParams() {
+        this.mainState.signState = "Начальное состояние, подготовка к соединению, создание исходного порта для связи с известным удаленным портом";
+        this.mainState.arrow = "first";
+        this.mainState.seq = generateRandomSeq();
+        this.mainState.cli = ["SYN"];
+    }
+
+    clickFirstButton() {
+        this.mainState.state = new SendFirstFlags(this.mainState);
+        this.mainState.render();
+    }
+
+    addButtonForSynRequest() {
+        $("#start-state-button").append(`<button type="button" id="send-first-flag-button" class="btn btn-primary first-button">Send SYN</button>`);
+    }
+}
+// ---------------------------------------------------------------------------------
+class SendFirstFlags {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+    }
+
+    setParams() {
+        this.mainState.signState = "Клиент выполнил запрос на создание TCP-сессии. Отправлен SYN-запроc)";
+        this.mainState.side = "server";
+        this.mainState.clientStatus = "SEND-SYN";
+        this.renderAcceptButton();
+        this.renderRejectButton();
+    }
+
+    clickFirstButton() {
+        this.mainState.state = new ServerReceived(this.mainState);
+        this.mainState.render();
+    }
+
+    clickSecondButton() {
+        this.mainState.state = new ServerRejectSynState(this.mainState);
+        this.mainState.render();
+    }
+
+    renderAcceptButton() {
+        $("#start-state-button").append(`<button type="button" id="received-syn-server" class="btn btn-success first-button">Accept</button>`);
+    }
+
+    renderRejectButton() {
+        $("#start-state-button").append(`<button type="button" id="reject-syn-server" class="btn btn-danger second-button">Reject</button>`);
+    }
+}
+// ---------------------------------------------------------------------------------
+class ServerReceived {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+        this.renderAckAndSynButtons();
+    }
+
+    setParams() {
+        this.mainState.signState = "Сервер получил сегмент, запомнил номер последовательности и создал сокет для обслуживания нового клиента.";
+        this.mainState.side = 'server';
+        this.mainState.arrow = 'second';
+        this.mainState.serverStatus = 'SYN-RECEIVED';
+        this.mainState.srcPort = this.mainState.portServer;
+        this.mainState.dstPort = this.mainState.portClient;
+        this.mainState.ack = generateRandomSeq();
+        this.mainState.seq++;
+        [this.mainState.seq, this.mainState.ack] = [this.mainState.ack, this.mainState.seq];
+        this.mainState.cli = ["ACK"];
+    }
+
+    clickFirstButton() {
+        this.mainState.state = new ClientGetAckAndSyn(this.mainState);
+        this.mainState.render();
+    }
+
+    clickSecondButton() {
+        this.mainState.state = new ClientGetAckAndSyn(this.mainState);
+        this.mainState.render();     
+    }
+
+    renderAckAndSynButtons() {
+        $("#start-state-button").append(`<button type="button" id="send-syn-ack" class="btn btn-success first-button">Send ACK SYN</button>`);
+    }
+}
+// ---------------------------------------------------------------------------------
+class ServerRejectSynState {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+        this.renderButtons();
+    }
+
+    setParams() {
+        this.mainState.signState = "Сервер не принял запрос";
+        this.mainState.arrow = "second";
+    }
+
+
+    clickFirstButton() {
+        this.mainState.state = new ClientGetRstFlag(this.mainState);
+        this.mainState.render();
+    }
+
+    clickSecondButton() {
+        this.mainState.state = new StartState(this.mainState); 
+        this.mainState.render();
+    }
+
+    renderButtons() {
+        $("#start-state-button").append(`<button type="button" id="send-rst" class="btn btn-success first-button">Send RST</button>`);
+        $("#start-state-button").append(`<button type="button" id="wait-nothing" class="btn btn-info second-button">Do nothing</button>`);
+    }
+}
+// ---------------------------------------------------------------------------------
+class ClientGetRstFlag {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+    }
+
+    setParams() {
+        this.mainState.signState = "Клиент получил RST-флаг, соединение разорвано";
+        this.mainState.srcPort = this.mainState.dstPort;
+        this.mainState.dstPort = this.mainState.srcPort;
+        this.mainState.cli = ["RST"];
+        this.mainState.side = "client";
+    }
+}
+// ---------------------------------------------------------------------------------
+class ClientGetAckAndSyn  {
+
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+        this.showButtonSendToServer();
+    }
+
+    setParams() {
+        this.mainState.seq = this.mainState.seq + 1;
+        this.mainState.srcPort = this.mainState.portClient;
+        this.mainState.dstPort = this.mainState.portServer;
+        this.mainState.arrow = "third";
+        this.mainState.signState = "Клиент получил сегмент с ACK и SYN";
+        this.mainState.side = "client";
+        this.mainState.clientStatus = "SYN-RECEIVED";
+        this.mainState.serverStatus = "SEND-SYN";
+    }
+
+    clickFirstButton() {
+        this.mainState.state = new EstablishedServer(this.mainState);
+        this.mainState.render();
+    }
+
+    showButtonSendToServer() {
+        $("#start-state-button").append(`<button type="button" id="final-state" class="btn btn-info first-button">Send ACK</button>`);
     }
 }
 
+// ------------------------------------------------
+class EstablishedServer {
+    constructor(mainState) {
+        this.mainState = mainState;
+        this.setParams();
+    }
 
+    setParams() {
+        this.mainState.signState = "Сервер в состоянии SYN-RECEIVED получил сегмент с флагом ACK. Соединение установлено";
+        this.mainState.srcPort = this.mainState.portClient;
+        this.mainState.dstPort = this.mainState.portServer;
+        this.mainState.cli = ["ACK"];
+        this.mainState.arrow = "two-side";
+        this.mainState.serverStatus = "Established";
+        this.mainState.clientStatus = "Established";
+        this.mainState.side = "server";
+    }
+}   
+// -------------------------------------------------------------------
+// Logic
+let tcp = new Main();
 
-let portClient = 54822;
-let portServer = 110;
-let seq = generateRandomSeq();
-let ack = generateRandomSeq();
-
-let state = new StartState();
-
-// Нажили на кнопку отправить флаги
-$(document).on ("click", "#send-first-flag-button",  () => {
-    state = new SendFirstFlags(seq, portClient, portServer);
+$(document).on ("click", ".first-button",  (e) => {
+    tcp.clickFirstButton(e.target);
+    console.log(tcp);
 });
 
-$(document).on ("click", "#received-syn-server", () => {
-    state = new ServerReceived();
+$(document).on ("click", ".second-button",  (e) => {
+    tcp.clickSecondButton(e.target);
+    console.log(tcp);
 });
-
-$(document).on ("click", "#reject-syn-server", () => {
-    state = new ServerRejectSynState();
-});
-
-$(document).on ("click", "#send-rst", () => {
-    state = new ClientGetRstFlag(seq, portClient, portServer);
-});
-
-$(document).on ("click", "#wait-nothing", () => {
-    state = new StartState(seq, portClient, portServer);
-});
-
-$(document).on ("click", "#send-syn-ack", () => {
-    state = new ClientGetAckAndSyn(seq, portClient, portServer, ack);
-});
-
-$(document).on ("click", "#final-state", () => {
-    state = new EstablishedServer(seq, portClient, portServer, ack);
-});
-
-
-// 
-
-// $("#received-syn-server").on('click', function() {
-//     alert(1);
-// });
